ee41fc6f0f430b5b82b144bd91eba244c5df71f6
nikhil-kumar787 <nikhilkumar7167@gmail.com>
2021-07-18 18:53:13 +0530

    minor changes in todocontroller

--- controller/todocontroller.js
+++ controller/todocontroller.js
@@ -41,19 +41,19 @@ exports.addtodo = async (req, res) => {
 
             });
 
-            todo.save((todo, error) => {
-                if (todo) {
-                    return res.status(201).json({
-                        message: "Successfully addded a Todo",
+            todo.save((error, todo) => {
+                if (error) {
+                    return res.status(400).json({
+                        message: "Something went wrong",
                     });
 
                 }
 
-                if (error) {
+                if (todo) {
 
 
                     return res.status(201).json({
-                        message: "Something went wrong"
+                        message: "Successfully addded a Todo"
                     });
                 }
 
